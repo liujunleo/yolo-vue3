d91c2c2586e6b77984e5efc7a2820cb971a61485
刘俊 <793898892@qq.com>
2023-08-14 16:35:00 +0800

    ✨第七回: 实现 isReadonly & isReactive 功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,10 @@
 import { mutableHandler, readonlyHandler } from './baseHandlers'
 
+export enum ReactiveFlags {
+    IS_REACTIVE = '__v__is_reactive',
+    IS_READONLY = '__v__is_readonly'
+}
+
 export function reactive(raw: Object) {
     return createActiveObject(raw, mutableHandler)
 }
@@ -10,4 +15,12 @@ export function readonly(raw) {
 
 export function createActiveObject(raw, baseHandler) {
     return new Proxy(raw, baseHandler)
+}
+
+export function isReactive (value){
+    return !!value[ReactiveFlags.IS_REACTIVE]
+}
+
+export function isReadonly (value){
+    return !!value[ReactiveFlags.IS_READONLY]
 }
