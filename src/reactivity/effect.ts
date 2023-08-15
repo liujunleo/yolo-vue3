22cf5fe57fba3b997b8895bd360f8853f2c0acfa
刘俊 <793898892@qq.com>
2023-08-15 13:31:10 +0800

    ✨第八回: 优化 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,19 +1,31 @@
 import { extend } from "../shared"
 
+const targetMap = new Map()
+let activeEffect
+let shouldTrack
+
 class ReactiveEffect {
-    public scheduler: Function | undefined
+    public scheduler?: Function
     private _fn: Function
     deps:any = [] 
     active = true
-    onStop?: Function | undefined
+    onStop?: Function
 
     constructor(fn: Function, scheduler?: Function) {
        this._fn = fn
        this.scheduler = scheduler
     }
     run() {
+        if(!this.active) {
+           return this._fn()
+        }
+
+        shouldTrack = true
         activeEffect = this
-        return this._fn()
+        const result = this._fn()
+        shouldTrack = false
+
+        return result
     }
     stop() {
         if(this.active) {
@@ -27,11 +39,53 @@ class ReactiveEffect {
 }
 
 function cleanupEffect (effect) {
-    // 
     effect.deps.forEach(dep => {
         dep.delete(effect)
     });
+    effect.deps.length = 0
 } 
+
+export function stop(runner) {
+    runner.effect.stop()
+}
+
+export function track(target, key) {
+    if(!isTracking()) return
+
+    //  将对象的依赖数组取出
+    let depsMap = targetMap.get(target)
+    if(!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap) // [ { user: count }: [] ]
+    }
+    // 将 key 的依赖取数
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Set()
+        depsMap.set(key, dep) // [ { user: count }: [ count: [] ] ]
+    }
+
+    if(dep.has(activeEffect)) return
+
+    dep.add(activeEffect) // [ { user: count }: [ count: [ ReactiveEffect, ReactiveEffect, ...] ] ]
+    activeEffect.deps.push(dep)
+}
+
+function isTracking() {
+   return shouldTrack && activeEffect !== undefined
+}
+
+export function trigger(target, key) {
+    const depsMap = targetMap.get(target)
+    const dep = depsMap.get(key)
+    for (const effect of dep) {
+        if (effect.scheduler) {
+            effect.scheduler()
+        } else {
+            effect.run()
+        }
+    }
+}
 
 export function effect(fn: Function, options:any = {}) {
     // fn
@@ -46,42 +100,4 @@ export function effect(fn: Function, options:any = {}) {
     const runner:any = _effect.run.bind(_effect)
     runner.effect = _effect
     return runner
-}
-
-export function stop(runner) {
-    runner.effect.stop()
-}
-
-const targetMap = new Map()
-let activeEffect: ReactiveEffect | undefined
-
-export function track(target, key) {
-    //  将对象的依赖数组取出
-    let depsMap = targetMap.get(target)
-    if(!depsMap) {
-        depsMap = new Map()
-        targetMap.set(target, depsMap) // [ { user: count }: [] ]
-    }
-    // 将 key 的依赖取数
-    let dep = depsMap.get(key)
-    if (!dep) {
-        dep = new Set()
-        depsMap.set(key, dep) // [ { user: count }: [ count: [] ] ]
-    }
-    if(!activeEffect) return
-    dep.add(activeEffect) // [ { user: count }: [ count: [ ReactiveEffect, ReactiveEffect, ...] ] ]
-    activeEffect.deps.push(dep)
-    activeEffect = undefined
-}
-
-export function trigger(target, key) {
-    const depsMap = targetMap.get(target)
-    const dep = depsMap.get(key)
-    for (const effect of dep) {
-        if (effect.scheduler) {
-            effect.scheduler()
-        } else {
-            effect.run()
-        }
-    }
 }
