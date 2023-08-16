79a6a254b8bb27ffa45fad936950a91217754d8e
刘俊 <793898892@qq.com>
2023-08-16 15:22:21 +0800

    ✨第十四回: 实现 proxyRefs 功能

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect"
 import { reactive } from "../reactive"
-import { isRef, ref, unRef } from "../ref"
+import { isRef, proxyRefs, ref, unRef } from "../ref"
 
 describe('ref', () => {
     it('happy path', () => {
@@ -54,10 +54,33 @@ describe('ref', () => {
         const origin = 'origin'
         const user = ref('user')
         const obj = ref({ age: 18 })
+        expect(unRef(1)).toBe(1)
         expect(unRef(origin)).toBe('origin')
         expect(unRef(user)).toBe('user')
         expect(unRef(obj)).toHaveProperty('age')
         expect(unRef(obj).age).toBe(18)
-        expect(unRef(1)).toBe(1)
+    })
+
+    it('proxyRefs', () => {
+        const origin = { 
+            age: ref(18),
+            name: 'leo' as any
+        }
+        const proxyUser = proxyRefs(origin)
+        expect(origin.age.value).toBe(18)
+        expect(proxyUser.age).toBe(18)
+        expect(proxyUser.name).toBe('leo')
+        // set normal value
+        proxyUser.age = 20
+        expect(proxyUser.age).toBe(20)
+        expect(origin.age.value).toBe(20)
+        // set ref value
+        proxyUser.age = ref(30)
+        expect(proxyUser.age).toBe(30)
+        expect(origin.age.value).toBe(30)
+        // change name to ref
+        proxyUser.name = ref('joe')
+        expect(proxyUser.name).toBe('joe')
+        expect(origin.name.value).toBe('joe')
     })
 })
