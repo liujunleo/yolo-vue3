6dbca498c03b9a143bc7a8b2dc7e0362f9045771
刘俊 <793898892@qq.com>
2023-08-17 13:55:47 +0800

    ✨第十五回: 实现 computed 功能

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -1,7 +1,12 @@
-// computed 返回 new ComputedRefImpl(getter)，在其实例 get value 时调用传入的 getter：computed(getter)
-// 每次访问 get value 时，以 _dirty 属性判断当前是否已缓存结果 _value，省略无意义的 getter
-// 利用 getter 中的响应对象发生改变 (set) 时，会 trigger 触发依赖的特性，将 getter 和 scheduler 重置函数传入，如： new ReactiveEffect(getter, () => this._dirty = true)
-// 将 “重置 _dirty 属性” 函数写入依赖中 scheduler，每次 set 后将 trigger 触发依赖，执行 scheduler 更新 _dirty，下次再调用 get value 时，判断 _dirty 并获取到最新值
+// **computed 功能**
+
+// 1. computed(getter) 返回 new ComputedRefImpl(getter)，在其 ComputedRefImpl 实例 get value 时调用传入的 getter
+
+// 2. 每次访问 get value 时，以 _dirty 属性判断当前是否已缓存结果 _value，省略无意义的 getter
+
+// 3. 利用 getter 中的响应式对象 get 时收集依赖（track），set 时触发依赖（trigger）的特性，将 getter 和 scheduler 重置函数传入 ReactiveEffect 实例，如： new ReactiveEffect(getter, () => this._dirty = true)
+
+// 4. 将 “重置 _dirty 属性” 函数写入依赖中 scheduler，每次 set 后将 trigger 触发依赖，执行此 scheduler 更新 _dirty，下次再调用 get value 时，将判断 _dirty 为初始值并执行 getter 以获取到最新值
 
 import { ReactiveEffect } from "./effect"
 
