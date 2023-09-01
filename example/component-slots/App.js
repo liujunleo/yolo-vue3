87d0ea5c09fb86d552dbec103a2e8ad6c4a4aeeb
刘俊 <793898892@qq.com>
2023-09-01 16:56:05 +0800

    ✨第二十四回: 实现组件 slots 功能

---
+++ example/component-slots/App.js
@@ -0,0 +1,40 @@
+import { h } from '../../lib/yolo-vue.esm.js'
+import { Foo } from './Foo.js'
+
+export const App = {
+    name: 'App',
+    render() {
+        const slots = {
+            default: () => h('p', {}, 'fooSlot - defualt'),
+            header: ({row, title}) => 
+                [
+                    h('p', {}, 'fooSlot - header'), 
+                    h('p', {}, `fooSlot - header - scoped: ${row.age} ${title}`)
+                ],
+            footer: () => h('p', {}, 'fooSlot - footer')
+        }
+        return h('div', { class: 'App' }, 
+            [
+                h('p', {}, 'App'), 
+                h(Foo, {}, slots)
+            ]
+        )
+        // 等同于 template：
+        // <div class="App">
+        //    <p>App</p>
+        //    <Foo>
+        //        <p>fooSlot - defualt</p>
+        //        <template #header={row, title}>
+        //            <p>fooSlot - header</p>
+        //            <p>fooSlot - header - scoped: {{ row.age }} {{ title }}</p>
+        //        </template>
+        //        <template v-slot:footer>
+        //            <p>fooSlot - footer</p>
+        //        </template>
+        //    </Foo>
+        // </div>
+    },
+    setup() {
+       
+    }
+}
--- src/shared/ShapeFlags.ts
+++ src/shared/ShapeFlags.ts
@@ -2,5 +2,6 @@ export enum ShapeFlags {
     ELEMENT = 1,
     STATEFUL_COMPONENT = 1 << 1,
     TEXT_CHILDREN = 1 << 2,
-    ARRAY_CHILREN = 1 << 3
+    ARRAY_CHILREN = 1 << 3,
+    SLOT_CHILDREN = 1 << 4
 }
